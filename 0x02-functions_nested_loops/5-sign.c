27f8b8e3d69347f97ca1f5dca7e941ad7b0a7bda
Chima Jude <chimajude914@gmail.com>
2023-07-19 19:06:19 -0700

    fnction,nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and print + if n is greaterv then zero
+ * 0 and print 0 if n is less than zero
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		-putchar('+');
+		return (1);
+	}else if (n == 0)	
+}
